cf01754b55905b5f2969c22ee828411594b20eaa
kayla177 <likayla177@gmail.com>
2023-12-29 18:22:25 -0500

    added album to type.ts

--- spotify/types.ts
+++ spotify/types.ts
@@ -1,13 +1,23 @@
 import Stripe from 'stripe';
 
 export interface Song {
-    id: string;
+    id: bigint | string;
     user_id: string;
     author: string;
     title: string;
     song_path: string;
     image_path: string;
 }
+
+export interface Album{
+    id: string;
+    user_id: string;
+    artist: string;
+    name: string;
+    album_path: string;
+    image_path: string;
+}
+
 
 export interface Product {
     id: string;
